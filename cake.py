67d655bffdbb5bf9046c2827e60e6813c47f8c0e
Sai Anumukonda <anumukondasrisaianumukonda@gmail.com>
2026-07-12 17:26:18 +0530

    Add files via upload

---
+++ cake.py
@@ -0,0 +1,29 @@
+#if-elif-else
+'''price=int(input("enter the price"))
+if(price==1200):
+          print("redvelevet cake")
+elif(price==1000):
+    print("almond cake")
+elif(price==800):
+    print("choclate cake")
+elif(price==600):
+    print("butter scotch")
+else:
+    print("cake is not available")'''
+    
+
+#if-elif-else
+name=input("enter the pizza name").lower()
+if(name == "bbq pizza"):
+    print('1000')
+elif(name == "crispy chicken pizza"):
+    print('800')
+elif(name == "paneer pizza"):
+    print('600')
+elif(name == "corn pizza"):
+    print('400')
+elif(name == "french fries and coke"):
+    print('200')
+else:
+    print("pizza is not available")
+
